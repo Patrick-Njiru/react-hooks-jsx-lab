7e6d17aa88dfd65c4237b0efcddeff3fe77487a1
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-09-21 18:55:29 +0300

    Completed assignment.

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        I have wanted to be a Software Developer ever since I interacted with a computer for the first time.</p>
+      <img src={image} alt="I made this"/>
+    </div>
+    )
 }
 
 export default About;
